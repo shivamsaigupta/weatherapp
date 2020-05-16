116969421d5e87b7536159f74f7c03a8be536ac7
Shivam Sai Gupta <contact@shivam.me>
2020-05-16 14:25:59 +0530

    Added snapshot testing

--- src/components/__test__/weather.test.js
+++ src/components/__test__/weather.test.js
@@ -5,6 +5,8 @@ import Weather from '../weather.component';
 import { render } from '@testing-library/react';
 import "@testing-library/jest-dom";
 
+import renderer from "react-test-renderer";
+
 it("renders without crashing", () => {
     const div = document.createElement("div");
     ReactDOM.render(<Weather />, div);
@@ -17,3 +19,14 @@ it("renders weather details correctly", () => {
     expect(getByTestId("precip")).toHaveTextContent("Sunny");
 })
 
+it("renders weather details correctly 2", () => {
+    const {getByTestId} = render(<Weather location="Delhi" temp="190" precip="Rain"></Weather> );
+    expect(getByTestId("location")).toHaveTextContent("Delhi");
+    expect(getByTestId("temp")).toHaveTextContent("190");
+    expect(getByTestId("precip")).toHaveTextContent("Rain");
+})
+
+it("matches snapshot", () => {
+    const tree = renderer.create(<Weather location="Mumbai" temp="234" precip="Sunny"></Weather> ).toJSON();    
+    expect(tree).toMatchSnapshot();
+})
